55e88d488f2f4c16ee2b5214f72c44e31a9cb43b
julian <julamifra@gmail.com>
2021-07-01 00:27:34 +0200

    modify server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static('./dist/pokemon-app-battle'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/pokemon-app-battle/'}),
+    res.sendFile('dist/pokemon-app-battle/index.html'),
 );
 
 // Start the app by listening on the default Heroku port
